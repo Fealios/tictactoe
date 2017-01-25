a8603e4454d1a9786427389cf456542416d20a7a
Melvin Gruschow and Nicholas Mead <nicholas@>
2017-01-25 15:01:10 -0800

    complete easy computer mode

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ var winner = false;
 var computer = false;
 
 function randomNum(){
-  return (Math.round(Math.random()*8));
+  var random = (Math.round(Math.random()*8));
+  while(newGame.moves.includes(random) && !winner){
+    random = (Math.round(Math.random()*8));
+  }
+  return random;
 }
 
 function checkWinner(){
@@ -45,6 +49,8 @@ function Board() {
   this.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   this.counter = 0;
   this.entry = "X";
+  this.moves = [];
+  this.computerSelect = '';
 }
 
 Board.prototype.iterate = function(){
@@ -56,22 +62,24 @@ Board.prototype.iterate = function(){
 }
 
 Board.prototype.computerTurn = function(){
-  var computerSelect = randomNum();
-  if(this.board[computerSelect] !== "X" && this.board[computerSelect] !== "O"){
-    this.[computerSelect] = this.entry;
-    checkWinner();
-    this.counter++;
-  }
+  this.iterate();
+  this.computerSelect = randomNum();
+  this.board[this.computerSelect] = this.entry
+  console.log(this.computerSelect)
+  this.moves.push(this.computerSelect);
+  checkWinner();
+  this.counter++;
 }
 
 $(document).ready(function(){
   newGame = new Board();
 
   $('#twoPlayerButton, #quit').click(function() {
-    $('#twoPlayer, .modeButton').toggleClass("hide");
+    $('#gameBoard, .modeButton').toggleClass("hide");
   });
 
   $('#singlePlayerButton').click(function(){
+    $('#gameBoard, .modeButton').toggleClass("hide");
     computer = true;
   })
 
@@ -84,24 +92,24 @@ $(document).ready(function(){
   })
 //================================================
   $('.space button').click(function() {
+
+    newGame.iterate();
+    $(this).hide();
+    $(this).next().text(newGame.entry);
+    var movePosition = parseInt($(this).val());
+    newGame.board[movePosition] = newGame.entry;
+    newGame.moves.push(movePosition);
+    checkWinner();
+    newGame.counter++;//end of player
+
     if(!computer){
-      newGame.iterate();
-      $(this).hide();
-      $(this).next().text(newGame.entry);
-      var userEntry=$(this).val();
-      newGame.board[parseInt(userEntry)] = newGame.entry;
-      checkWinner();
-      newGame.counter++;
+      console.log('you are playing against a human');
     }
     else if(computer){
-      newGame.iterate();
-      $(this).hide();
-      $(this).next().text(newGame.entry);
-      var userEntry=$(this).val();
-      newGame.board[parseInt(userEntry)] = newGame.entry;
-      checkWinner();
-      newGame.counter++;
-      computerTurn();
+      console.log('you are playing against a machine');
+      newGame.computerTurn();
+      $('#' + newGame.computerSelect).hide();
+      $('#' + newGame.computerSelect).next().text(newGame.entry);
     }
   });
 })
